7d38942d0c39b45bca2990fce26ab4f0c85caafe
Jan Milosh <janmilosh@gmail.com>
2014-02-23 21:15:37 -0500

    Small content tweak, and adds anchorScroll();

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,4 +36,9 @@ angular.module('ketoApp', [
     .otherwise({
       redirectTo: '/'
     });
+}).run(function($rootScope, $location, $anchorScroll, $routeParams) {
+  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+    $location.hash($routeParams.scrollTo);
+    $anchorScroll();  
+  });
 });
