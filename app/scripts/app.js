0f07f4aee79598649407c53e7903589711853577
Jan Milosh <janmilosh@gmail.com>
2014-02-25 00:18:25 -0500

    Partial redesign with 2-column layout and multi-color scheme

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,7 +36,8 @@ angular.module('ketoApp', [
     .otherwise({
       redirectTo: '/'
     });
-}).run(function($rootScope, $location, $anchorScroll, $routeParams) {
+})
+.run(function($rootScope, $location, $anchorScroll, $routeParams) {
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
     $location.hash($routeParams.scrollTo);
     $anchorScroll();  
